2fc38170f4f6b4db9a55e0f5dc1bbdf2ed984eb5
Leo <leo.amadobrito32@gmail.com>
2022-12-03 14:20:22 +0000

    .

--- main.R
+++ main.R
@@ -1,2 +1,3 @@
 # Read a txt file
 data <- read.table(file = "Report1_Dataset.txt", header = TRUE) 
+colnames(data) = c("id", "lenght", "age", "infection_risk", "culturing_ratio", "chest_xray", "beds", "school", "region", "avg_census", "nr_nurses", "facilities_services")
